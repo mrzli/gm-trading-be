e55577826a1bd093fdf0c79db2edf6f597a59bed
Goran Mržljak <goran.mrzljak@gmail.com>
2024-01-22 13:36:25 +0100

    fixes related to saving trade state

--- src/modules/trade/trade.service.ts
+++ src/modules/trade/trade.service.ts
@@ -1,6 +1,10 @@
 import { NonRequiredOnly } from './../../types/generic/generic';
 import { Injectable } from '@nestjs/common';
-import { TickerDataResolution, TradeState } from '@gmjs/gm-trading-shared';
+import {
+  TickerDataResolution,
+  TickerDataSource,
+  TradeState,
+} from '@gmjs/gm-trading-shared';
 import { TradeStateRepository } from '../db/repositories';
 import { parseIntegerOrThrow } from '@gmjs/number-util';
 import { DbTradeState } from '../db/types';
@@ -27,7 +31,7 @@ function dbTradeStateToTradeState(dbTradeState: DbTradeState): TradeState {
   return {
     userId: dbTradeState.user_id.toString(),
     saveName: dbTradeState.save_name,
-    tickerDataSource: dbTradeState.ticker_data_source,
+    tickerDataSource: dbTradeState.ticker_data_source as TickerDataSource,
     tickerName: dbTradeState.ticker_name,
     tickerResolution: dbTradeState.ticker_resolution as TickerDataResolution,
     timezone: dbTradeState.timezone,
